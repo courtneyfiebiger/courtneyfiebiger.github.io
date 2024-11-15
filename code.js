490f9eb4bb11c4dd2084e06586481dd41518ef06
courtneyfiebiger <courtney.fiebiger@ndus.edu>
2024-11-14 21:51:39 -0600

    Add files via upload

--- code.js
+++ code.js
@@ -1,7 +1,29 @@
-
-document.getElementById("b1").addEventListener("click", onclick)
-
-function onclick (){
-     
-    document.getElementById("heading").innerHTML = document.getElementById("data").value
-}
+let canvas_element = document.getElementById("c");
+let painter = canvas_element.getContext('2d');
+let canvas_element2 = document.getElementById("d");
+let painter2 = canvas_element2.getContext('2d');
+painter.fillStyle = "#000000";
+painter.fillRect(0,0,400,400);
+painter2.fillStyle = "#000000";
+painter2.fillRect(0,0,400,400);
+painter.fillStyle = "#FF0000";
+painter2.fillStyle = "#FF0000";
+let w =20, h=20,gap=10;
+for (let r =0; r <13; ++r){
+    for(let c = r; c < 13; ++c) {
+        let x = gap + c * (w+gap);
+        let y = gap + r * (h + gap);
+        painter.fillRect(x,y,w,h);}}
+let r =0;
+while (r <13){
+    for(let c = r; c < 13; ++c) {
+        let x = gap + c * (w+gap);
+        let y = gap + r * (h + gap);
+        painter2.fillRect(x,y,w,h);  
+        ++r}
+    for (let r =0; r <13; ++r){
+        for(let c = 0; c < 13; ++c) {
+            let x = gap + r * (w+gap);
+            let y = gap + c * (h + gap);  
+            painter2.fillRect(x,y,w,h)
+        }}}
